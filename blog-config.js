13cbe4b9df31ddc1ae960273e4e26860b9b596b6
zunione <xtwish1@ajou.ac.kr>
2025-10-16 13:38:01 +0900

    Initial commit from gatsby: (https://github.com/zunione/gatsby-starter-hoodie.git)

---
+++ blog-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: "hoodie",
+  description: "Hello :) I'm Hudi who developed gatsby-starter-hoodie theme.",
+  author: "Hudi",
+  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  links: {
+    github: "https://github.com/devHudi",
+    linkedIn: "https://linkedin.com",
+    facebook: "https://www.facebook.com",
+    twitter: "https://twitter.com",
+    x: "https://x.com",
+    instagram: "https://www.instagram.com",
+    medium: "https://medium.com",
+    blogger: "https://blogger.com",
+    email: "mailto:devhudi@gmail.com",
+    resume: "https://hudi.blog",
+    link: "https://hudi.blog",
+  },
+  useAbout: true,
+  // See https://giscus.app/
+  giscus: {
+    repo: "devHudi/gatsby-starter-hoodie",
+    repoId: "MDEwOlJlcG9zaXRvcnkzNjk4NjMzNTg=",
+    category: "Comments",
+    categoryId: "DIC_kwDOFguqvs4ChwGy",
+    mapping: "pathname",
+    strict: "0",
+    reactionsEnabled: "1",
+    inputPosition: "bottom",
+    lang: "en",
+  },
+}
